ef0e1c9973ad653e10777487beb82fca543e194f
Milen Khanal <milenkhanal@gmail.com>
2019-08-08 19:36:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,9 +4,9 @@
 #     expect(reverse_each_word(sentence1)).to eq("olleH ,ereht dna woh era ?uoy")
 #   end
         # hash.gsub(/(\[\"|\"\])/, '').split('", "')
-
+require 'pry'
 def reverse_each_word(sentence)
       my_array = sentence.split
  my_array.reverse_each{|x| print x, " " }
- 
-end
+ binding.pry
+ end
