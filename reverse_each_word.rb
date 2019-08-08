65f6e061168b34645a350248566d1c51ff0716dd
Milen Khanal <milenkhanal@gmail.com>
2019-08-08 18:36:58 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+# describe '#reverse_each_word' do
+#   let(:sentence1) { "Hello there, and how are you?" }
+#   it 'reverses all the words in a string without reversing the order of the words' do
+#     expect(reverse_each_word(sentence1)).to eq("olleH ,ereht dna woh era ?uoy")
+#   end
+        # hash.gsub(/(\[\"|\"\])/, '').split('", "')
+
+def reverse_each_word(hash)
+      my_array = hash.split
+ 
+
+end
